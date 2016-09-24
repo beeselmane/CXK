d6f306e460d2021cdabd0ba6d1d7e2ade66200a1
beeselmane <beeselmane@gmail.com>
2016-09-24 12:50:11 -0400

    **Fixed** Build 000F [SL v0.1, SLSetupKernel.elf v0.1]

--- CXSystemLoader.c
+++ CXSystemLoader.c
@@ -9,7 +9,7 @@
 #define kCXLoaderVersionB '1'
 
 #define kCXLoaderVersion "A.1"
-#define kCXLoaderBuild   "000E"
+#define kCXLoaderBuild   "000F"
 
 #define kCXLoaderStartTextU CXUTF16String("Corona-X System Loader Version " kCXLoaderVersion " [Build " kCXLoaderBuild "]\r\n")
 
@@ -151,9 +151,9 @@ UTF16String CXSystemLoaderGetLoaderPath(CXHandle imageHandle, CXSystemTable syst
     return CXDevicePathToString(image->filePath, true, false);
 }
 
-void CLSerialWrite(const char *string)
+void CLSerialWrite(const char *s)
 {
-    while (s) CXKBasicSerialPortWriteCharacter(gSerialPort0, *s++, true);
+    while (s) CXKBasicSerialWriteCharacter(gSerialPort0, *s++, true);
 }
 
 void noSerialCheck()
@@ -165,8 +165,8 @@ void noSerialCheck()
     printf(CXUTF16String("Serial Base Address: 0x%p\r\n"), port);
     printf(CXUTF16String("Serial Scratch Port: 0x%p\r\n"), &port->scratchSpace);
 
-    CXKWriteByteToPort(&port->scratchSpace, testValue);
-    UInt8 portValue = CXKReadByteFromPort(&port->scratchSpace);
+    CXKWriteByte(&port->scratchSpace, testValue);
+    UInt8 portValue = CXKReadByte(&port->scratchSpace);
 
     printf(CXUTF16String("Wrote 0x%02X, Read 0x%02X\r\n"), testValue, portValue);
     printf(CXUTF16String("Serial Port Unaccessable.\r\n\r\n"));
@@ -174,22 +174,22 @@ void noSerialCheck()
 
 void CXSerialSetup()
 {
-    CXKBasicSerialPortSetupLineControl(gSerialPort0, kCXKBasicSerialWordLength8Bits, kCXKBasicSerial1StopBit, kCXKBasicSerialNoParity);
-    CXKBasicSerialPortSetBaudRate(gSerialPort0, 57600);
+    CXKBasicSerialSetupLineControl(gSerialPort0, kCXKBasicSerialWordLength8Bits, kCXKBasicSerial1StopBit, kCXKBasicSerialNoParity);
+    CXKBasicSerialSetBaudRate(gSerialPort0, 57600);
 
 #if 0
-    UInt8 interruptsEnabled = CXKReadByteFromPort(&gSerialPort0->byte1.interruptsEnabled);
-    UInt8 interruptInfo = CXKReadByteFromPort(&gSerialPort0->byte2.interruptInfo);
-    UInt8 lineControl = CXKReadByteFromPort(&gSerialPort0->lineControl);
-    UInt8 modemControl = CXKReadByteFromPort(&gSerialPort0->modemControl);
-    UInt8 lineStatus = CXKReadByteFromPort(&gSerialPort0->lineStatus);
-    UInt8 modemStatus = CXKReadByteFromPort(&gSerialPort0->modemStatus);
-    UInt8 scratchSpace = CXKReadByteFromPort(&gSerialPort0->scratchSpace);
+    UInt8 interruptsEnabled = CXKReadByte(&gSerialPort0->byte1.interruptsEnabled);
+    UInt8 interruptInfo = CXKReadByte(&gSerialPort0->byte2.interruptInfo);
+    UInt8 lineControl = CXKReadByte(&gSerialPort0->lineControl);
+    UInt8 modemControl = CXKReadByte(&gSerialPort0->modemControl);
+    UInt8 lineStatus = CXKReadByte(&gSerialPort0->lineStatus);
+    UInt8 modemStatus = CXKReadByte(&gSerialPort0->modemStatus);
+    UInt8 scratchSpace = CXKReadByte(&gSerialPort0->scratchSpace);
 
-    CXKWriteByteToPort(&gSerialPort0->lineControl, lineControl | (1 << kCXKBasicSerialDivisorExposeShift));
-    UInt8 divisorLSB = CXKReadByteFromPort(&gSerialPort0->byte0.divisorLSB);
-    UInt8 divisorMSB = CXKReadByteFromPort(&gSerialPort0->byte1.divisorMSB);
-    CXKWriteByteToPort(&gSerialPort0->lineControl, lineControl);
+    CXKWriteByte(&gSerialPort0->lineControl, lineControl | (1 << kCXKBasicSerialDivisorExposeShift));
+    UInt8 divisorLSB = CXKReadByte(&gSerialPort0->byte0.divisorLSB);
+    UInt8 divisorMSB = CXKReadByte(&gSerialPort0->byte1.divisorMSB);
+    CXKWriteByte(&gSerialPort0->lineControl, lineControl);
 
     printf(CXUTF16String("Discovered Serial Port at 0x%p:\r\n"), gSerialPort0);
     printf(CXUTF16String("IE [0x%p]: %02X\r\n"), &gSerialPort0->byte1.interruptsEnabled, interruptsEnabled);
@@ -204,7 +204,7 @@ void CXSerialSetup()
     printf(CXUTF16String("\r\n"));
 #endif
 
-    const char *startText = {'C', 'X', 'B', 'L', 'v', kCXLoaderVersionA, '.', kCXLoaderVersionB, '\r', '\n'};l
+    const char startText[11] = {'C', 'X', 'B', 'L', 'v', kCXLoaderVersionA, '.', kCXLoaderVersionB, '\r', '\n', '\0'};
     CLSerialWrite(startText);
 }
 
@@ -271,7 +271,7 @@ CXStatus CXEFI CXSystemLoaderMain(input CXHandle imageHandle, input CXSystemTabl
     CXKSetPOSTValue(0xFF);
 
     printf(kCXLoaderStartTextU);
-    gSerialPort0 = CXKBasicSerialPortIntializeAtAddress((OSAddress)kCXKBasicSerialDefaultOffset);
+    gSerialPort0 = CXKBasicSerialIntializeAtAddress((OSAddress)kCXKBasicSerialDefaultOffset);
 
     if (!gSerialPort0) noSerialCheck();
     else               CXSerialSetup();
